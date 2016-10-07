e7bfcafb6e118f6cf60ac3a264e0fc99f53f9963
Dawei Wei <davwei@microsoft.com>
2016-10-07 11:57:36 -0700

    Add theme darktooth

--- lisp/.emacs-custom.el
+++ lisp/.emacs-custom.el
@@ -3,7 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(custom-safe-themes
+   (quote
+    ("c05b3e1761ba96b8169e62fd9c1a7359844a4c274f6879b6105984719f5fe8d7" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" default)))
+ '(package-selected-packages
+   (quote
+    (darktooth-theme which-key counsel swiper company popwin youdao-dictionary magit spacegray-theme))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
